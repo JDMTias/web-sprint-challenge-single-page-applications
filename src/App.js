f6eb72e07fefd1ed331692a0f3bd9f19b843af70
JDMTias <matiasiturbide@gmail.com>
2020-06-19 16:10:08 -0600

    chugging along

--- src/App.js
+++ src/App.js
@@ -1,18 +1,84 @@
-import React,{useState} from "react";
-import {BrowserRouter as Router, Route} from 'react-router-dom'
-import Order from './Order'
-import OrderForm from './OrderForm'
+import React, { useState, useEffect } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Order from "./Order";
+import OrderForm from "./OrderForm";
+import formSchema from "./Validation";
+import * as yup from "yup";
+
+const initialFormValues = {
+  name: "",
+  size: "",
+  toppings: {
+    goomba: false,
+    koopa: false,
+    blooper: false,
+    cheepcheep: false,
+    bowser: false,
+  },
+};
+
+const initialDisabled = true;
 
 const App = () => {
-  const [pizza, setPizza]=useState([])
-  console.log(pizza)
+  const [formValues, setFormValues] = useState([initialFormValues]);
+  console.log(formValues)
+  const [pizza, setPizza] = useState([]);
+  const [errorMessage, setError] = useState({});
+  const [disabled, setDisabled] = useState(initialDisabled);
+
+  const onInputChange = (event) => {
+
+    const currentField = event.target.name;
+    const value = event.target.value;
+
+    yup
+      .reach(formSchema, currentField)
+      .validate(value)
+      .then(valid => {
+        setError({ ...errorMessage, [currentField]:"" });
+      })
+      .catch((error) => {
+        setError({ ...errorMessage, [currentField]: error.errors[0] });
+      });
+    setFormValues({ ...formValues, [currentField]: value });
+  };
+
+  const onCheckboxChange = (event) => {
+    const {name, checked}= event.target;
+    setFormValues({ ...formValues, [name]: checked });
+  };
+
+  const onSubmitHandler = (event) => {
+    event.preventDefault();
+    const newPizza = {
+      name: formValues.name.trim(),
+      size: formValues.size,
+      toppings: Object.keys(formValues.toppings)
+       .filter(toppingName => (formValues.toppings[toppingName]===true)) ,
+      special: formValues.special.trim(),
+    }
+    setPizza(newPizza)
+  };
+
+
+  useEffect(() => {
+    
+    formSchema.isValid(formValues).then(valid => {
+      setDisabled(!valid)
+    })
+  }, [formValues])
+
+
   return (
     <Router>
-    <div>
-      <h1>Mario's Pizza</h1>
-  <Route path='/' exact component={()=><Order/>}/>
-  <Route path={'/pizza'} component={() =><OrderForm pizza={pizza} setPizza={setPizza}/>}/>
-    </div>
+      <div>
+        <h1>Mario's Pizza</h1>
+        <Route path="/" exact component={() => <Order />} />
+        <Route
+          path="/pizza">
+          <OrderForm pizza={pizza} setPizza={setPizza} onInputChange={onInputChange} onSubmitHandler={onSubmitHandler} onCheckboxChange ={onCheckboxChange} values={formValues} />
+        </Route>
+      </div>
     </Router>
   );
 };
--- src/OrderForm.js
+++ src/OrderForm.js
@@ -1,30 +1,86 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 // need to add a home button here as well that takes you to the homepage
 
-export default function OrderForm (props) {
+export default function OrderForm(props) {
+  const { pizza, setPizza, disabled, onInputChange,onSubmitHandler, onCheckboxChange, values } = props;
 
-    const {pizza, setPizza} = props
+  return (
+    <form onSubmit={onSubmitHandler}>
+      <label>Name: </label>
+      <input
+        type="text"
+        placeholder="Your Name"
+        name="name"
+        onChange={onInputChange}
+        value={values.name}
+      />
+      <label>Pizza Size:</label>
+      <select name="size">
+        <option value="placeholder">Choose Size</option>
+        <option value="10">10 inch</option>
+        <option value="14">14 inch</option>
+        <option value="18">Super Mario (18 inch)</option>
+      </select>
+      <div>
+        <h4>Toppings:</h4>
+        <label>
+          Goomba
+          <input
+            name="goomba"
+            type="checkbox"
+            onChange={onCheckboxChange}
+            checked={values.goomba}
+          />
+        </label>
 
+        <label>
+          Koopa
+          <input
+            name="koopa"
+            type="checkbox"
+            onChange={onCheckboxChange}
+            checked={values.koopa}
+          />
+        </label>
 
+        <label>
+          Blooper
+          <input
+            name="blooper"
+            type="checkbox"
+            onChange={onCheckboxChange}
+            checked={values.blooper}
+          />
+        </label>
 
-return(
-    <form>
-        <label>Name: </label>
-        <input
-            type="text"
-            placeholder="Your Name"
-            name="name"
-            // onChange={}
-        />
-        <label>Pizza Size:</label>
-        <select name='size'>
-            <option value="placeholder">Choose Size</option>
-            <option value="10">10 inch</option>
-            <option value="14">14 inch</option>
-            <option value="18">Super Mario (18 inch)</option>
-        </select>
-        
+        <label>
+          cheep cheep
+          <input
+            name="cheepcheep"
+            type="checkbox"
+            onChange={onCheckboxChange}
+            checked={values.cheepcheep}
+          />
+        </label>
 
+        <label>
+          Bowser
+          <input
+            name="bowser"
+            type="checkbox"
+            onChange={onCheckboxChange}
+            checked={values.bowser}
+          />
+        </label>
+      </div>
+      <label>Special Instructions </label>
+      <input
+        type="text"
+        placeholder="The princess is in a different castle"
+        name="special"
+        onChange={onInputChange}
+      />
+    <button disabled={disabled} type='submit'>Add to Order</button>
     </form>
-)
+  );
 }
